d7cff405260fe33fcf8f3806d0f40ff9df0978a2
Diego-Dlh <125380009+Diego-Dlh@users.noreply.github.com>
2024-12-26 18:11:36 -0500

    Update config.py
    me equivoque otra vez

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'una_llave_secreta_para_flask'
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://gestionresiduosbd_user:MXfrd6DNzK0o2iRzdWnvubhDuuAnW7Az@dpg-ctmtthhopnds73fildpg-a/gestionresiduosbd"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://gestionresiduosbd_user:MXfrd6DNzK0o2iRzdWnvubhDuuAnW7Az@dpg-ctmtthhopnds73fildpg-a/gestionresiduosbd")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
