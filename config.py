92855facfaaa2d5b011095c57828c1498013ed60
Diego-Dlh <125380009+Diego-Dlh@users.noreply.github.com>
2024-12-26 17:59:41 -0500

    Update config.py
    cambio para red

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'una_llave_secreta_para_flask'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+pg8000://postgres:123456@localhost/Sistema_recoleccion'
-
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://gestionresiduosbd_user:MXfrd6DNzK0o2iRzdWnvubhDuuAnW7Az@dpg-ctmtthhopnds73fildpg-a/gestionresiduosbd)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
