82a7ce29c63b5b3b597ff26d3c5da49fb266c583
Diego-Dlh <diegodelahozb@hotmail.com>
2025-04-04 09:10:52 -0500

    update database URI to point to new gestionresiduosbdv4 instance

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     #SQLALCHEMY_DATABASE_URI = 'postgresql+pg8000://postgres:123456@localhost/Sistema_recoleccion' #base de datos de prueba
     #SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://gestionresiduosbd_user:MXfrd6DNzK0o2iRzdWnvubhDuuAnW7Az@dpg-ctmtthhopnds73fildpg-a/gestionresiduosbd")
     #SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://gestionresiduosbdv2_user:xCmhebEXoYnHf4J2fdgCwRqYy0JdwL4F@dpg-cu9qvvaj1k6c73e0eotg-a/gestionresiduosbdv2")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://gestionresiduosbdv3_user:sF6oVaaaYUEK6va43TKE9ZgyWdinTDci@dpg-cv3omhofnakc73eqt7h0-a/gestionresiduosbdv3")
+    #SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://gestionresiduosbdv3_user:sF6oVaaaYUEK6va43TKE9ZgyWdinTDci@dpg-cv3omhofnakc73eqt7h0-a/gestionresiduosbdv3")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://gestionresiduosbdv4_user:AMKxipc87M9gD2notkHyrITnKJQXgukN@dpg-cvnub17gi27c73bp0rt0-a/gestionresiduosbdv4")
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
